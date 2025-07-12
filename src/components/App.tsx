c65699fb9aef8a388556d04a2f5f066c8eef9319
Vitaliy Khyzhnyak <vit.khyzhn@gmail.com>
2025-07-12 23:29:20 +0300

    adjust structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
 import css from './App.module.css';
-import CafeInfo from './components/CafeInfo/CafeInfo';
-import Notification from './components/Notification/Notification';
-import VoteOptions from './components/VoteOptions/VoteOptions';
-import VoteStats from './components/VoteStats/VoteStats';
+import CafeInfo from './CafeInfo/CafeInfo';
+import Notification from './Notification/Notification';
+import VoteOptions from './VoteOptions/VoteOptions';
+import VoteStats from './VoteStats/VoteStats';
 import { useState } from 'react';
-import type { Votes, VoteType } from './types/votes';
+import type { Votes, VoteType } from '../types/votes';
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
