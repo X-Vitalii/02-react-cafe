29e0dbcd91884fc932a986f779336a99fb1ece0f
Vitaliy Khyzhnyak <vit.khyzhn@gmail.com>
2025-07-12 23:34:59 +0300

    adjust structure - App folder

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import css from './App.module.css';
-import CafeInfo from './CafeInfo/CafeInfo';
-import Notification from './Notification/Notification';
-import VoteOptions from './VoteOptions/VoteOptions';
-import VoteStats from './VoteStats/VoteStats';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import Notification from '../Notification/Notification';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
 import { useState } from 'react';
-import type { Votes, VoteType } from '../types/votes';
+import type { Votes, VoteType } from '../../types/votes';
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
